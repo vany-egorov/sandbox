13b871aa8aa68165cef7bf71678701229d347d46
Ivan Egorov <vany.egorov@gmail.com>
2015-12-01 21:05:20 +0000

    buggy but work fifo

--- c-cpp/4-upd-mpegts/fifo.c
+++ c-cpp/4-upd-mpegts/fifo.c
@@ -14,6 +14,7 @@ FIFO *fifo_new(size_t cap) {
 	it->len = 0;
 	it->start = 0;
 	it->finish = 0;
+	sem_init(&it->sem, 0, 0);
 
 	return it;
 }
@@ -71,12 +72,16 @@ void fifo_delete(FIFO *it) {
 	}
 }
 
-size_t fifo_write(FIFO *it, uint8_t *src, size_t src_len) {
-	if (!it->data) it->data = (uint8_t*)calloc(1, src_len);
+void fifo_wait_data(FIFO *it) {
+	sem_wait(&it->sem);
+}
 
+size_t fifo_write(FIFO *it, uint8_t *src, size_t src_len) {
 	size_t finish_prv = it->finish;
 	size_t finish_nxt = (it->finish + (size_t)src_len) % it->cap;
 
+	pthread_mutex_lock(&it->rw_mutex);
+	if (!it->data) it->data = (uint8_t*)calloc(1, src_len);
 	void *result = memcpy(it->data + it->finish, src, src_len);
 
 	it->finish = finish_nxt;
@@ -87,15 +92,27 @@ size_t fifo_write(FIFO *it, uint8_t *src, size_t src_len) {
 		it->start = it->finish;
 	}
 
+	sem_post(&it->sem);
+	pthread_mutex_unlock(&it->rw_mutex);
+
 	return src_len;
 }
 
-size_t fifo_read(FIFO *it, uint8_t *dst, size_t dst_len) {
+void fifo_read(FIFO *it, uint8_t *dst, size_t dst_len, size_t *readed_len) {
+	pthread_mutex_lock(&it->rw_mutex);
+
+	if (!it->len) {
+		*readed_len = 0;
+		pthread_mutex_unlock(&it->rw_mutex);
+		return;
+	}
+
 	void *result = memcpy(dst, it->data, dst_len);
 	memset(it->data, 0, dst_len);
 
 	it->start = (it->start + (size_t)dst_len) % it->cap;
 	it->len -= dst_len;
+	pthread_mutex_unlock(&it->rw_mutex);
 
-	return dst_len;
+	*readed_len = dst_len;
 }
--- c-cpp/4-upd-mpegts/fifo.h
+++ c-cpp/4-upd-mpegts/fifo.h
@@ -4,20 +4,24 @@
 
 #include <string.h> // memcpy, size_t
 #include <stdint.h> // uint8_t, uint16_t
+#include <semaphore.h> // sem_t
 
 
 typedef struct _FIFO {
 	uint8_t *data;
 	size_t len, cap,
 	       start, finish;
+	sem_t sem;
+	pthread_mutex_t rw_mutex;
 } FIFO;
 
 FIFO *fifo_new(size_t cap);
 void fifo_print(FIFO *it);
 void fifo_print_data(FIFO *it);
 void fifo_delete(FIFO *it);
+void fifo_wait_data(FIFO *it);
 size_t fifo_write(FIFO *it, uint8_t *src, size_t src_len);
-size_t fifo_read(FIFO *it, uint8_t *dst, size_t dst_len);
+void fifo_read(FIFO *it, uint8_t *dst, size_t dst_len, size_t *readed_len);
 
 
 #endif // __FIFO__
