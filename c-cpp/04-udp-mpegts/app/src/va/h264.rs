eb340115fe411a522368ae3fb742778c41e132ce
Ivan Egorov <vany.egorov@gmail.com>
2016-12-09 13:19:28 +0000

    move libva wrapper to mod va;

---
+++ c-cpp/04-udp-mpegts/app/src/va/h264.rs
@@ -0,0 +1,9 @@
+#[repr(C)]
+pub struct H264 {
+    pub _1_: u64,
+    pub _2_: u64,
+    pub _3_: u64,
+    pub _4_: u64,
+    pub _5_: u64,
+    pub _6_: u64,
+}
---
+++ c-cpp/04-udp-mpegts/app/src/va/url.rs
@@ -0,0 +1,24 @@
+extern crate libc;
+
+use libc::c_char;
+
+
+#[repr(C)]
+pub struct URL {
+    pub scheme: u32,
+    pub port: u16,
+    pub buf: [c_char; 255usize],
+    pub buf_len: u16,
+    pub _bindgen_bitfield_1_: u8,
+    pub _bindgen_bitfield_2_: u8,
+    pub _bindgen_bitfield_3_: u8,
+    pub _bindgen_bitfield_4_: u8,
+    pub _bindgen_bitfield_5_: u8,
+    pub _bindgen_bitfield_6_: u8,
+    pub pos_userinfo: u16,
+    pub pos_host: u16,
+    pub pos_path: u16,
+    pub pos_query: u16,
+    pub pos_fragment: u16,
+    pub flags: u8,
+}
