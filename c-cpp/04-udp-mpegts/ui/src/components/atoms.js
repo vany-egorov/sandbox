30d6e9daadef6dcc776be686790242189189021c
Ivan Egorov <vany.egorov@gmail.com>
2017-02-26 23:25:13 +0300

    post-css

--- c-cpp/04-udp-mpegts/ui/src/components/atoms.js
+++ c-cpp/04-udp-mpegts/ui/src/components/atoms.js
@@ -7,96 +7,56 @@ class Atom extends React.Component {
   render() {
     const model = this.props.model
 
-    const classes = {
-      'atoms-grid__row': true
-    }
+    let cellName = ''
+    let cellData = ''
 
-    switch (model.atomKind) {
-    case ak.H264SPS:
-      classes['atoms-grid__row_h264-sps'] = true
+    const classes = {'atoms-grid__row': true}
 
-      return (
-        <div className={classNames(classes)}>
-          0x{model.offset.toString(16)}&nbsp;
-            {model.sz}&nbsp;
-            H264 {ak.toString(model.atomKind)}
-        </div>
-      )
-    case ak.H264PPS:
-      classes['atoms-grid__row_h264-pps'] = true
+    if (ak.isH264(model.atomKind)) {
+      cellName = `H264 ${ ak.toString(model.atomKind) }`
 
-      return (
-        <div className={classNames(classes)}>
-          0x{model.offset.toString(16)}&nbsp;
-            {model.sz}&nbsp;
-            H264 {ak.toString(model.atomKind)}
-        </div>
-      )
-    case ak.H264AUD:
-      classes['atoms-grid__row_h264-aud'] = true
+      if (model.atomKind == ak.H264SPS) {
+        classes['atoms-grid__row-h264-sps'] = true
+      } else if (model.atomKind == ak.H264PPS) {
+        classes['atoms-grid__row-h264-pps'] = true
+      } else if (model.atomKind == ak.H264AUD) {
+        classes['atoms-grid__row-h264-aud'] = true
+      } else if (model.atomKind == ak.H264SEI) {
+        classes['atoms-grid__row-h264-sei'] = true
+      } else if (model.atomKind == ak.H264SliceIDR) {
+        cellName = `H264 ${h264NALSliceType.toString(model.sliceType)}`
+        cellData = `{ \
+        frame-num: ${model.frameNum}, \
+        pic-order-cnt-lsb: ${model.picOrderCntLsb}\
+        }`
 
-      return (
-        <div className={classNames(classes)}>
-          0x{model.offset.toString(16)}&nbsp;
-            {model.sz}&nbsp;
-            H264 {ak.toString(model.atomKind)}
-        </div>
-      )
-    case ak.H264SEI:
-      classes['atoms-grid__row_h264-sei'] = true
+        classes['atoms-grid__row-h264-slice-idr'] = true
 
-      return (
-        <div className={classNames(classes)}>
-          0x{model.offset.toString(16)}&nbsp;
-            {model.sz}&nbsp;
-            H264 {ak.toString(model.atomKind)}
-        </div>
-      )
-    case ak.H264SliceIDR:
-      classes['atoms-grid__row_h264-slice-idr'] = true
-
-      switch (model.sliceType) {
-      case h264NALSliceType.I:
-      case h264NALSliceType.SI:
-      case h264NALSliceType.I2:
-      case h264NALSliceType.SI2:
-        classes['atoms-grid__row_h264-slice-idr_i'] = true
-        break
-
-      case h264NALSliceType.P:
-      case h264NALSliceType.SP:
-      case h264NALSliceType.P2:
-      case h264NALSliceType.SP2:
-        classes['atoms-grid__row_h264-slice-idr_p'] = true
-        break
-
-      case h264NALSliceType.B:
-      case h264NALSliceType.B2:
-        classes['atoms-grid__row_h264-slice-idr_b'] = true
-        break
+        if (h264NALSliceType.isI(model.sliceType)) {
+          classes['atoms-grid__row-h264-slice-idr-i'] = true
+        } else if (h264NALSliceType.isP(model.sliceType)) {
+          classes['atoms-grid__row-h264-slice-idr-p'] = true
+        } else if (h264NALSliceType.isB(model.sliceType)) {
+          classes['atoms-grid__row-h264-slice-idr-b'] = true
+        }
       }
-
-      return (
-        <div className={classNames(classes)}>
-          0x{model.offset.toString(16)}&nbsp;
-            {model.sz}&nbsp;
-            H264 {h264NALSliceType.toString(model.sliceType)} slice #0 (
-            frame-num: {model.frameNum},
-            pic-order-cnt-lsb: {model.picOrderCntLsb}
-          )
-        </div>
-      )
-    default:
-      return (
-        <div className={classNames(classes)}>
-          0x{model.offset.toString(16)}&nbsp;
-            {model.sz}&nbsp;
-            H264 {ak.toString(model.atomKind)}
-        </div>
-      )
     }
-  }
-}
+
+    return (
+      <div className={classNames(classes)}>
+        <div className="atoms-grid__cell atoms-grid__cell-offset">
+          0x{model.offset.toString(16)}
+        </div>
+        <div className="atoms-grid__cell atoms-grid_cell-name">
+          {cellName}
+        </div>
+        <div className="atoms-grid__cell atoms-grid_cell-data">
+          {cellData}
+        </div>
+      </div>
+    )
+  } // render
+} // class
 
 class Atoms extends React.Component {
   createChildComponent(model) {
