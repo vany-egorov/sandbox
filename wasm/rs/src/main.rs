d30fd952b14753489385fea0d756ee8e5af5ff5b
Ivan Egorov <vany.egorov@gmail.com>
2018-06-05 01:22:41 +0300

    tiny wasm example;

--- wasm/rs/src/main.rs
+++ wasm/rs/src/main.rs
@@ -1,13 +1,43 @@
-#[macro_use]
 extern crate stdweb;
+
+
+use stdweb::web::{
+    document,
+    IParentNode,  // query_selector
+    INode,  // append_child
+};
+
 
 fn main() {
     stdweb::initialize();
 
-    let message = "Hello, 世界!";
-    js! {
-        alert( @{message} );
-    }
+    let body = document()
+        .query_selector("body")
+        .unwrap()
+        .unwrap();
+
+    let div1 = document().create_element("div").unwrap();
+    let h1 = document().create_element("h1").unwrap();
+
+    let div11 = document().create_element("div").unwrap();
+    let div111 = document().create_element("div").unwrap();
+    let div112 = document().create_element("div").unwrap();
+    let div113 = document().create_element("div").unwrap();
+    let input_src = document().create_element("input").unwrap();
+    let input_dst = document().create_element("input").unwrap();
+    let button = document().create_element("button").unwrap();
+    h1.set_text_content("rendered using rust!");
+    button.set_text_content("exchange");
+
+    div1.append_child(&h1);
+    div1.append_child(&div11);
+    div11.append_child(&div111);
+    div111.append_child(&input_src);
+    div11.append_child(&div112);
+    div112.append_child(&input_dst);
+    div11.append_child(&div113);
+    div113.append_child(&button);
+    body.append_child(&div1);
 
     stdweb::event_loop();
 }
